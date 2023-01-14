924e3589ae0aa7cdccadd9e12d8f1bd38de2b6ab
Wen Junhua <wenjh1998@gmail.com>
2023-01-14 14:01:42 +0800

    feat: add homepage test

--- orderonus_be/homepage/tests.py
+++ orderonus_be/homepage/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class HomepageTest(TestCase):
+    def test_homepage(self: "HomepageTest") -> None:
+        """Check if the homepage returns a 200 status code"""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "Server is up and running"})
--- orderonus_be/homepage/views.py
+++ orderonus_be/homepage/views.py
@@ -2,6 +2,6 @@ from django.http.request import HttpRequest
 from django.http.response import HttpResponse, JsonResponse
 
 # Create your views here.
-def homepage(request: HttpRequest) -> HttpResponse:
+def homepage(_: HttpRequest) -> HttpResponse:
     """Home page to check server liveness"""
     return JsonResponse({"message": "Server is up and running"})
