a3b2595f6f7170605992f13b8300eb7758cd971d
Wen Junhua <wenjh1998@gmail.com>
2023-01-15 05:10:08 +0800

    fix: update auth to use json

--- orderonus_be/auth/tests.py
+++ orderonus_be/auth/tests.py
@@ -26,7 +26,11 @@ class LoginTest(TestCase):
 
     def test_login_fail_no_username(self: "LoginTest") -> None:
         """Check if the login fails if there is no username"""
-        response = self.client.post("/accounts/login/", {"password": self.password})
+        response = self.client.post(
+            "/accounts/login/",
+            {"password": self.password},
+            content_type="application/json",
+        )
         self.assertEqual(response.status_code, 400, response.content)
         self.assertEqual(
             response.json(), {"error": "Username or password not provided"}
@@ -34,7 +38,11 @@ class LoginTest(TestCase):
 
     def test_login_fail_no_password(self: "LoginTest") -> None:
         """Check if the login fails if there is no password"""
-        response = self.client.post("/accounts/login/", {"username": self.username})
+        response = self.client.post(
+            "/accounts/login/",
+            {"username": self.username},
+            content_type="application/json",
+        )
         self.assertEqual(response.status_code, 400, response.content)
         self.assertEqual(
             response.json(), {"error": "Username or password not provided"}
@@ -43,7 +51,9 @@ class LoginTest(TestCase):
     def test_login_fail_user_does_not_exist(self: "LoginTest") -> None:
         """Check if the login fails correctly if the user does not exist"""
         response = self.client.post(
-            "/accounts/login/", {"username": "test2", "password": self.password}
+            "/accounts/login/",
+            {"username": "test2", "password": self.password},
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, 404, response.content)
         self.assertEqual(
@@ -53,7 +63,9 @@ class LoginTest(TestCase):
     def test_login_fail_user_password_incorrect(self: "LoginTest") -> None:
         """Check if the login fails correctly if the user password is wrong"""
         response = self.client.post(
-            "/accounts/login/", {"username": self.username, "password": "test2"}
+            "/accounts/login/",
+            {"username": self.username, "password": "test2"},
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, 404, response.content)
         self.assertEqual(
@@ -63,7 +75,9 @@ class LoginTest(TestCase):
     def test_login_success(self: "LoginTest") -> None:
         """Check if the login works"""
         response = self.client.post(
-            "/accounts/login/", {"username": self.username, "password": self.password}
+            "/accounts/login/",
+            {"username": self.username, "password": self.password},
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, 200, response.content)
         self.assertEqual(response.json(), {"data": "User logged in successfully"})
@@ -83,7 +97,9 @@ class RegisterTest(TestCase):
     def test_register_fail_duplicate_username(self: "RegisterTest") -> None:
         """Test if the registration fails if there is a duplicated username"""
         response = self.client.post(
-            "/accounts/register/", {"username": self.username, "password": "1234"}
+            "/accounts/register/",
+            {"username": self.username, "password": "1234"},
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, 409, response.content)
         self.assertEqual(response.json(), {"error": "User already exists"})
@@ -95,14 +111,22 @@ class RegisterTest(TestCase):
 
     def test_register_fail_no_username(self: "RegisterTest") -> None:
         """Test if the registration fails if there is no username"""
-        response = self.client.post("/accounts/register/", {"password": "1234"})
+        response = self.client.post(
+            "/accounts/register/",
+            {"password": "1234"},
+            content_type="application/json",
+        )
         self.assertEqual(response.status_code, 400, response.content)
         self.assertEqual(
             response.json(), {"error": "Username or password not provided"}
         )
 
     def test_register_fail_no_password(self: "RegisterTest") -> None:
-        response = self.client.post("/accounts/register/", {"username": "test"})
+        response = self.client.post(
+            "/accounts/register/",
+            {"username": "test"},
+            content_type="application/json",
+        )
         self.assertEqual(response.status_code, 400, response.content)
         self.assertEqual(
             response.json(), {"error": "Username or password not provided"}
@@ -113,7 +137,9 @@ class RegisterTest(TestCase):
         username = "test2"
         password = "password"
         response = self.client.post(
-            "/accounts/register/", {"username": username, "password": password}
+            "/accounts/register/",
+            {"username": username, "password": password},
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, 201, response.content)
         self.assertEqual(response.json(), {"data": "User created successfully"})
--- orderonus_be/auth/views.py
+++ orderonus_be/auth/views.py
@@ -1,7 +1,8 @@
+import json
 from django.contrib.auth.models import User
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse, JsonResponse
-from django.views.decorators.http import require_GET, require_POST
+from django.views.decorators.http import require_POST
 from django.contrib.auth import authenticate, login
 
 
@@ -9,13 +10,12 @@ from django.contrib.auth import authenticate, login
 @require_POST
 def user_login(request: HttpRequest) -> HttpResponse:
     """Log in for the user"""
-    username = request.POST.get("username", None)
-    password = request.POST.get("password", None)
+    post_dict = json.loads(request.body)
+    username = post_dict.get("username", None)
+    password = post_dict.get("password", None)
 
     if (None in [username, password]) or ("" in [username, password]):
-        return JsonResponse(
-            {"error": "Username or password not provided"}, status=400
-        )
+        return JsonResponse({"error": "Username or password not provided"}, status=400)
 
     user = authenticate(request, username=username, password=password)
     if user is None:
@@ -31,12 +31,11 @@ def user_login(request: HttpRequest) -> HttpResponse:
 @require_POST
 def user_register(request: HttpRequest) -> HttpResponse:
     """Register the user"""
-    username = request.POST.get("username", None)
-    password = request.POST.get("password", None)
+    post_dict = json.loads(request.body)
+    username = post_dict.get("username", None)
+    password = post_dict.get("password", None)
     if (None in [username, password]) or ("" in [username, password]):
-        return JsonResponse(
-            {"error": "Username or password not provided"}, status=400
-        )
+        return JsonResponse({"error": "Username or password not provided"}, status=400)
 
     user = User.objects.filter(username=username).first()
     if user is not None:
